82186ebcbd36b4fe4069a116f62290bad3756b4a
selvadhanush <selva.dhanush.s81@kalvium.community>
2025-09-27 18:23:59 +0530

    Contact-page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Contact from "./components/Contact/Contact";
+
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <Router>
+      <Routes>
+   
+
+        
+        <Route path="/contact" element={<Contact />} />
+
+       
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;

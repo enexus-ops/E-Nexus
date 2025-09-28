238b68747aa59bbe6e43c47888a336bb90c6d722
PoshikaM <poshika.m@kalvium.community>
2025-09-28 10:37:31 +0530

    did event endpoints in backend

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const express = require('express')
 const cors = require('cors')
+const eventRoutes = require('./eventRoutes');
 require('dotenv').config()
 const app = express()
 const PORT = process.env.PORT || 5000
@@ -8,6 +9,8 @@ const PORT = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 const uri = process.env.MONGO_URI
+
+app.use('/api/events', eventRoutes);
 
 mongoose.connect(uri)
 .then(()=>{

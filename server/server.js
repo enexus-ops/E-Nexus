ee13dbb1173c21d08f15536b08b124f790452789
Sahanashre-V <sahanashre.v@gmail.com>
2025-09-30 22:44:20 +0530

    Done with sending email in contact page

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const eventRoutes = require('./eventRoutes');
 require('dotenv').config()
+const { sendContactEmail } = require('./email');
 const app = express()
 const PORT = process.env.PORT || 5000
 
@@ -11,6 +12,16 @@ app.use(express.json())
 const uri = process.env.MONGO_URI
 
 app.use('/api/events', eventRoutes);
+
+app.post('/api/contact', async (req, res) => {
+  try {
+    const info = await sendContactEmail(req);
+    res.json({ ok: true, messageId: info.messageId });
+    console.log('Full info:', info);
+  } catch (err) {
+    res.status(400).json({ ok: false, error: err.message });
+  }
+});
 
 mongoose.connect(uri)
 .then(()=>{
